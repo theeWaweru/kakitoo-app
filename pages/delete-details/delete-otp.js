4d47dabd6113347c9a212d4991b8e817b1f3e261
theeWaweru <102231510+itsQuinces@users.noreply.github.com>
2022-07-07 12:30:54 +0300

    8th commit

--- pages/delete-details/delete-otp.js
+++ pages/delete-details/delete-otp.js
@@ -50,9 +50,9 @@ export default function app() {
                   numInputs={4}
                   isInputNum={true}
                   shouldAutoFocus={true}
-                  {...register("otp", {
-                    required: "Kindly key in the OTP sent to your phone",
-                  })}
+                  // {...register("otp", {
+                  //   required: "Kindly key in the OTP sent to your phone",
+                  // })}
                   inputStyle={{
                     border: "1px solid #4d4d4d",
                     borderRadius: "8px",
